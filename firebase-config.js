023c5d6b37d23f6b62b80e53f612a50d2481bf04
RunVision <contatomariofarias@gmail.com>
2026-05-31 23:41:47 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Web App config loaded by `firebase.js`.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBVlas9NyoE1nrVO7CHM57VSeif6mFVpYI",
+  authDomain: "coachsystem-c8206.firebaseapp.com",
+  projectId: "coachsystem-c8206",
+  storageBucket: "coachsystem-c8206.firebasestorage.app",
+  messagingSenderId: "1019727670793",
+  appId: "1:1019727670793:web:78d810844fd0a1bdcdd2d8",
+  measurementId: "",
+  appCheckSiteKey: "",
+};
